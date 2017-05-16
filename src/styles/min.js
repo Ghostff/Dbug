3fad0727d5094a496276d0634c17b04d177c90e9
ghostff <frankchris@hotmail.com>
2017-05-16 02:05:02 -0500

    oops

--- src/styles/min.js
+++ src/styles/min.js
@@ -1,7 +1,5 @@
 
-(function (window, undefined) {
-
-
+(function () {
     function setCookie(a, b) {
         var c = new Date;
         c.setTime(c.getTime() + 24 * b * 60 * 60 * 1e3), document.cookie = a + "=" + b + ";expires=" + c.toUTCString()
@@ -34,6 +32,7 @@
         document.querySelector(".content-body ." + id + ".loops").classList.add("active");
         last_active = this;
     });
+
 
     var last_toggled = document.querySelector(".attr.middle #proc-main");
     var panel = document.querySelector(".attr.middle #repop");
@@ -75,7 +74,11 @@
         else { panel.style.display = "none";}
     });
 
-    document.getElementById(getCookie("BittrDebug_toggle_right")).nextElementSibling.style.display = 'block';
+
+    var last_cookie = getCookie("BittrDebug_toggle_right");
+    if (last_cookie) {
+        document.getElementById(last_cookie).nextElementSibling.style.display = 'block';
+    }
     delegate(document, "click", ".global .labeled", function() {
         setCookie("BittrDebug_toggle_right", this.getAttribute("id"));
         var x = this.nextElementSibling;
@@ -86,6 +89,7 @@
         }
     });
 
+
     delegate(document, "click", ".contents .right .global .listed .caret", function() {
         var m = this.nextElementSibling;
         if (m.style.display === 'none') {
@@ -95,57 +99,7 @@
         }
     });
 
-
-
-    /**
-     * @class window.tinyscrollbar
-     * @constructor
-     * @param {Object} [$container] Element to attach scrollbar to.
-     * @param {Object} options
-     @param {String} [options.axis='y'] Vertical or horizontal scroller? ( x || y ).
-     @param {Boolean} [options.wheel=true] Enable or disable the mousewheel.
-     @param {Boolean} [options.wheelSpeed=40] How many pixels must the mousewheel scroll at a time.
-     @param {Boolean} [options.wheelLock=true] Lock default window wheel scrolling when there is no more content to scroll.
-     @param {Number} [options.touchLock=true] Lock default window touch scrolling when there is no more content to scroll.
-     @param {Boolean|Number} [options.trackSize=false] Set the size of the scrollbar to auto(false) or a fixed number.
-     @param {Boolean|Number} [options.thumbSize=false] Set the size of the thumb to auto(false) or a fixed number
-     @param {Boolean} [options.thumbSizeMin=20] Minimum thumb size.
-     */
-    var tinyscrollbar = function($container, options) {
-        return new Plugin($container, options);
-    };
-
-    if(typeof define == 'function' && define.amd) {
-        define(function(){ return tinyscrollbar; });
-    }
-    else if(typeof module === 'object' && module.exports) {
-        module.exports = tinyscrollbar;
-    }
-    else {
-        window.tinyscrollbar = tinyscrollbar;
-    }
-
-
-
-})(window);
-
-window.onload = function()
-{
-    // The plain javascript api is very similar to the jquery version with some exceptions.
-    // There is no chaining like in the jquery api. So when you create a instance it
-    // will return all methods and properties.
-    //
-    var $scrollbar = document.getElementById("proc-main")
-        ,   scrollbar  = tinyscrollbar($scrollbar)
-    ;
-    // You can now call methods liks scrollbar.update() or scrollbar.contentPosition
-    // Here is a example how you can bind the move event to the scrollbar container.
-    //
-    $scrollbar.addEventListener("move", function()
-    {
-        console.log(scrollbar.contentPosition);
-    }, false);
-}
+})();
 
 
 
