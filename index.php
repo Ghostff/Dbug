4fe2e73ba455004da3515f79a2aa4b1cfd3bb45c
Chrys Ugwu <chrys@ghostff.com>
2017-05-17 11:46:12 -0500

    lots much calculation

--- index.php
+++ index.php
@@ -3,11 +3,12 @@
 
 
 spl_autoload_register(function ($name) {
-    include str_replace(['\\', 'Debug'], [DIRECTORY_SEPARATOR, 'src'], $name) . '.php'; #include str_replace(['\\', 'Debug'], [DIRECTORY_SEPARATOR, 'src'], $name) . '.php';
+    include str_replace(['\\', 'Debug'], [DIRECTORY_SEPARATOR, 'src'], $name) . '.php';
+    #include str_replace(['\\', 'Debug'], [DIRECTORY_SEPARATOR, 'src'], $name) . '.php';
 
 });
 
-new Debug\BittrDbug(\Debug\BittrDbug::PRETTIFY, 'yola', 45);
+new Debug\BittrDbug(\Debug\BittrDbug::PRETTIFY, 'default', 10);
 
 echo 111;
 class Foo
@@ -23,6 +24,7 @@ $_POST = ['foo' => 22, 'bar' => new Foo()];
 
 function renderError()
 {
+
     trigger_error("Cannot divide by zero", E_USER_ERROR);
 }
 function duplicateKey()
@@ -30,40 +32,88 @@ function duplicateKey()
     renderError();
 }
 
+function a()
+{
+    duplicateKey();
+}
 
+function b()
+{
+    a();
+}
 
+function c()
+{
+    b();
+}
+
+function d()
+{
+    c();
+}
+
+function e()
+{
+    d();
+}
+
+function f()
+{
+    e();
+}
+
+function i()
+{
+    f();
+}
+
+function j()
+{
+    i();
+}
+
+function k()
+{
+    j();
+}
+
+function l()
+{
+    k();
+}
+
+function m()
+{
+    l();
+}
+
+function n()
+{
+    m();
+}
+
+function o()
+{
+    n();
+}
+
+function p()
+{
+    o();
+}
+
+function q()
+{
+    p();
+}
+
+q();
 
 var_dump(new Foo());
 #duplicateKey();;
-\BittrEHandler\Modules\Dump::error();
+//\BittrEHandler\Modules\Dump::error();
 
 
 
-class Enum {
-    protected $self = array();
-    public function __construct( /*...*/ ) {
-        $args = func_get_args();
-        for( $i=0, $n=count($args); $i<$n; $i++ )
-            $this->add($args[$i]);
-    }
-
-    public function __get( /*string*/ $name = null ) {
-        return $this->self[$name];
-    }
-
-    public function add( /*string*/ $name = null, /*int*/ $enum = null ) {
-        if( isset($enum) )
-            $this->self[$name] = $enum;
-        else
-            $this->self[$name] = end($this->self) + 1;
-    }
-}
-
-class DefinedEnum extends Enum {
-    public function __construct( /*array*/ $itms ) {
-        foreach( $itms as $name => $enum )
-            $this->add($name, $enum);
-    }
-}
 
 
--- src/styles/min.js
+++ src/styles/min.js
@@ -10,94 +10,6 @@
         return b ? b[2] : null
     }
 
-    function delegate(el, evt, sel, handler) {
-        el.addEventListener(evt, function(event) {
-            var t = event.target;
-            while (t && t !== this) {
-                if (t.matches(sel)) {
-                    handler.call(t, event);
-                }
-                t = t.parentNode;
-            }
-        });
-    }
-
-    var last_active = document.querySelector('#cont-nav .top-tog.active');
-    delegate(document, "click", ".top-tog", function() {
-        var id = this.getAttribute("id");
-        this.classList.add("active");
-        var last_id = last_active.getAttribute("id");
-        last_active.classList.remove("active");
-        document.querySelector(".content-body ." + last_id + ".loops").classList.remove("active");
-        document.querySelector(".content-body ." + id + ".loops").classList.add("active");
-        last_active = this;
-    });
-
-
-    var last_toggled = document.querySelector(".attr.middle #proc-main");
-    var panel = document.querySelector(".attr.middle #repop");
-    delegate(document, "click", ".loop-tog", function() {
-        var id = this.getAttribute("data-id");
-        last_toggled.style.display = "none";
-        last_toggled = document.querySelector(".attr.middle #" + id);
-        last_toggled.style.display = "block";
-
-        var a = this.getAttribute("data-id");
-        var b = this.getAttribute("data-class");
-        var c = this.getAttribute("data-type");
-        var d = this.getAttribute("data-function");
-        var e = this.getAttribute("title");
-        var f = this.getAttribute("data-file");
-        var g = this.getAttribute("data-line");
-
-        if ("proc-buffer" != a) {
-            var h = "";
-            if (b) {
-                if (d) {
-                    h += '<div class="keyword">Class: ' + b + '</span><span style="margin: 3px;">' + c + '</span><span class="char-null">' + d + '</span>';
-                } else {
-                    h += '<div class="keyword">Function: <span class="char-null">' + b + '</span>';
-                }
-                h += '<span class="">()</span></div>';
-            } else {
-                h += '<div class="keyword">Class: <span class="char-null">null</span></div>';
-            }
-            if (e) {
-                e = e.replace(/\\/g, '<b class="char-object"> \\ </b>');
-                h += '<div class="namespace">Namespace: ' + e + '</div>';
-            } else {
-                h += '<div class="namespace">Namespace: <span class="char-null">null</span></div>';
-            }
-            h += '<div class="file">File: <span class="">' + f + '</span>:<span class="char-integer">' + g + '</span></div>';
-            panel.innerHTML = h;panel.style.display = "block";
-        }
-        else { panel.style.display = "none";}
-    });
-
-
-    var last_cookie = getCookie("BittrDebug_toggle_right");
-    if (last_cookie) {
-        document.getElementById(last_cookie).nextElementSibling.style.display = 'block';
-    }
-    delegate(document, "click", ".global .labeled", function() {
-        setCookie("BittrDebug_toggle_right", this.getAttribute("id"));
-        var x = this.nextElementSibling;
-        if (x.style.display === 'none') {
-            x.style.display = 'block';
-        } else {
-            x.style.display = 'none';
-        }
-    });
-
-
-    delegate(document, "click", ".contents .right .global .listed .caret", function() {
-        var m = this.nextElementSibling;
-        if (m.style.display === 'none') {
-            m.style.display = 'block';
-        } else {
-            m.style.display = 'none';
-        }
-    });
 
     var raf = w.requestAnimationFrame || w.setImmediate || function(c) { return setTimeout(c, 0); };
 
@@ -177,14 +89,10 @@
 
     ss.prototype = {
         moveBar: function(e) {
-            console.log(this.mg);
             var totalHeight = this.el.scrollHeight,
                 ownHeight = this.el.clientHeight,
                 _this = this;
-
             this.scrollRatio = ownHeight / totalHeight;
-
-            console.log(_this.mg);
             raf(function() {
                 if(_this.scrollRatio >= 1) {
                     _this.bar.classList.add('ss-hidden')
@@ -232,7 +140,129 @@
         var inner = e.clientHeight;
         e.style.display = 'none';
         e.setAttribute("style", "height:" + ((inner > 500) ? 600 : inner)  + "px;" + (styled ? styled : ''));
-        SimpleScrollbar.initEl(e, 14);
+        SimpleScrollbar.initEl(e, 16);
+    });
+
+    var height = 35 + getPos(document.querySelector('.left .content-nav')).y;
+    document.querySelectorAll('.left .loops').forEach(function(e) {
+        e.setAttribute("style", "width: 102%;height: calc(100vh - " + height + "px);");
+        SimpleScrollbar.initEl(e, 13);
+    });
+
+    function delegate(el, evt, sel, handler) {
+        el.addEventListener(evt, function(event) {
+            var t = event.target;
+            while (t && t !== this) {
+                if (t.matches(sel)) {
+                    handler.call(t, event);
+                }
+                t = t.parentNode;
+            }
+        });
+    }
+
+
+    var last_active = document.querySelector('#cont-nav .top-tog.active');
+    delegate(document, "click", ".top-tog", function() {
+        var id = this.getAttribute("id");
+        this.classList.add("active");
+        var last_id = last_active.getAttribute("id");
+        last_active.classList.remove("active");
+        document.querySelector(".content-body ." + last_id + ".loops").classList.remove("active");
+        document.querySelector(".content-body ." + id + ".loops").classList.add("active");
+        last_active = this;
+    });
+
+    var _cache;
+    function _atg(_lt) {
+
+        if (typeof _cache != "undefined") {
+            _cache.setAttribute("style", "height:0px;");
+        }
+        var lh = _lt.clientHeight;
+        var tbl = _lt.querySelector('table');
+        var tbl_h = tbl.clientHeight;
+        if (tbl_h == lh) {
+            return;
+        }
+
+        if (tbl_h < lh) {
+            var lmap = tbl.querySelector('.last-map');
+            lmap.setAttribute("style", "height:" + (lh - tbl_h) + "px;");
+        }
+        _cache = lmap;
+    }
+
+    var last_toggled = document.querySelector(".attr.middle #proc-main");
+    var panel = document.querySelector(".attr.middle #repop");
+
+    _atg(last_toggled);
+
+    delegate(document, "click", ".loop-tog", function() {
+        var id = this.getAttribute("data-id");
+        last_toggled.style.display = "none";
+        last_toggled = document.querySelector(".attr.middle #" + id);
+        last_toggled.style.display = "block";
+
+        var a = this.getAttribute("data-id");
+        var b = this.getAttribute("data-class");
+        var c = this.getAttribute("data-type");
+        var d = this.getAttribute("data-function");
+        var e = this.getAttribute("title");
+        var f = this.getAttribute("data-file");
+        var g = this.getAttribute("data-line");
+
+        console.log(a);
+        if ("proc-buffer" != a) {
+
+            _atg(last_toggled);
+            var h = "";
+            if (b) {
+                if (d) {
+                    h += '<div class="keyword">Class: ' + b + '</span><span style="margin: 3px;">' + c + '</span><span class="char-null">' + d + '</span>';
+                } else {
+                    h += '<div class="keyword">Function: <span class="char-null">' + b + '</span>';
+                }
+                h += '<span class="">()</span></div>';
+            } else {
+                h += '<div class="keyword">Class: <span class="char-null">null</span></div>';
+            }
+            if (e) {
+                e = e.replace(/\\/g, '<b class="char-object"> \\ </b>');
+                h += '<div class="namespace">Namespace: ' + e + '</div>';
+            } else {
+                h += '<div class="namespace">Namespace: <span class="char-null">null</span></div>';
+            }
+            h += '<div class="file">File: <span class="">' + f + '</span>:<span class="char-integer">' + g + '</span></div>';
+            panel.innerHTML = h;
+            panel.style.display = "block";
+        }
+        else { panel.style.display = "none";}
+    });
+
+
+    var last_cookie = getCookie("BittrDebug_toggle_right");
+    if (last_cookie) {
+        document.getElementById(last_cookie).nextElementSibling.style.display = 'block';
+    }
+    delegate(document, "click", ".global .labeled", function() {
+        setCookie("BittrDebug_toggle_right", this.getAttribute("id"));
+        var x = this.nextElementSibling;
+        if (x.style.display === 'none') {
+            x.style.display = 'block';
+        } else {
+            x.style.display = 'none';
+        }
+    });
+
+
+    delegate(document, "click", ".contents .right .global .listed .caret", function() {
+        var m = this.nextElementSibling;
+        if (m.style.display === 'none') {
+            m.style.display = 'block';
+        } else {
+            m.style.display = 'none';
+        }
     });
 
 
